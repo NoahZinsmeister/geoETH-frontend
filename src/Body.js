1c6e7adbe143c453e4b55ff899988242f4dd20e8
Andy <andychorlian@gmail.com>
2018-09-08 13:42:02 +0200

    Added chatroom support initial commit

--- src/Body.js
+++ src/Body.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { withWeb3 } from 'web3-webpacked-react';
-import { Typography } from '@material-ui/core'
+import { Typography } from '@material-ui/core';
+
+import Message from './Message';
 
 
 class Body extends Component {
   render() {
     return (
+      <div>
       <Typography align="center">Account: {this.props.w3w.account}</Typography>
+      <Message></Message>
+      </div>
     )
   }
 }
