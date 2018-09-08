cdc75ddb29fa9432cc13721752265c4508c79531
Andy <andychorlian@gmail.com>
2018-09-08 18:23:37 +0200

    Moving out stuff

---
+++ src/CustomCard.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+import blockies from 'blockies-identicon/blockies'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+import { withWeb3 } from 'web3-webpacked-react';
+
+import BlockiesIdenticon from './BlockiesIdenticon'
+import TransactionButton from './TransactionButton'
+
+const divMargin = {
+  margin: '8px'
+};
+
+class CustomCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log(this.props.user)
+    return (
+    <Card>
+      <div style={divMargin}>
+        <BlockiesIdenticon seed={this.props.user}/>
+      </div>
+      <CardContent>
+        <Typography color="textSecondary">
+          {this.props.user}
+        </Typography>
+        <TransactionButton
+          buttonInitial="Tip 0.01 ETH"
+          method={this.props.w3w.web3js.eth.sendTransaction({from: this.props.me, to: this.props.user, value: 10000000000000000})}
+        />
+      </CardContent>
+    </Card>
+    )
+  }
+}
+
+export default withWeb3(CustomCard)
--- src/Message.js
+++ src/Message.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import socketIOClient from 'socket.io-client'
-import Avatar from '@material-ui/core/Avatar'
 import blockies from 'blockies-identicon/blockies'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
+import { withWeb3 } from 'web3-webpacked-react';
+
 import { Widget, addResponseMessage, dropMessages, renderCustomComponent } from 'react-chat-widget';
 
+import CustomCard from './CustomCard'
+import BlockiesIdenticon from './BlockiesIdenticon'
+import TransactionButton from './TransactionButton'
+import ClickableBlockies from './ClickableBlockies'
 import 'react-chat-widget/lib/styles.css';
-
-const divMargin = {
-  margin: '8px'
-};
 
 class Message extends Component {
 
@@ -50,7 +51,7 @@ class Message extends Component {
           //   scale: 3,
           //   spotcolor: '#000'
           // }, canv);
-          renderCustomComponent(ClickableBlockies, {user: data.user})
+          renderCustomComponent(ClickableBlockies, {user: data.user, me: this.props.user})
           addResponseMessage(data.msg)
           // renderCustomComponent(CustomCard, {user: this.props.user})
         }
@@ -82,69 +83,4 @@ class Message extends Component {
 
 }
 
-class CustomCard extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-    <Card>
-      <div style={divMargin}>
-        <BlockiesIdenticon seed={this.props.user}/>
-      </div>
-      <CardContent>
-        <Typography color="textSecondary">
-          {this.props.user}
-        </Typography>
-      </CardContent>
-    </Card>
-    )
-  }
-}
-
-class ClickableBlockies extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  handleOnClick = () => {
-    renderCustomComponent(CustomCard, {user: this.props.user})
-  }
-
-  render() {
-    return (
-    <div onClick={this.handleOnClick}>
-      <BlockiesIdenticon seed={this.props.user}/>
-    </div>)
-  }
-}
-
-class BlockiesIdenticon extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  getOpts () {
-    console.log(this.props.seed)
-    return {
-      seed: this.props.seed || "foo",
-      color: '#ba3434',
-      bgcolor: '#dfe527',
-      size: 8,
-      scale: 3,
-      spotcolor: '#000'
-    };
-  }
-  componentDidMount() {
-    this.draw();
-  }
-  draw() {
-    blockies.render(this.getOpts(), this.canvas);
-  }
-  render() {
-    return React.createElement("canvas", {ref: canvas => this.canvas = canvas});
-  }
-}
-
-export default Message;
+export default withWeb3(Message);
